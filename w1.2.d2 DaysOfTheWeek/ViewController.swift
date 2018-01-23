fe0464be7e455f4d00535405b25fa5b46db8152b
Michael R.F <mrflowers22@uwalumni.com>
2018-01-23 10:06:37 -0700

    Initial Commit

---
+++ w1.2.d2 DaysOfTheWeek/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  w1.2.d2 DaysOfTheWeek
+//
+//  Created by Michael Flowers on 1/23/18.
+//  Copyright © 2018 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
